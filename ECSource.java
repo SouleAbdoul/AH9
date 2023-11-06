fe076b7a00e05f4daf8c66896a5ec9706aaa51b3
souleabdoul <souley.abdoulaziz@fiserv.com>
2023-11-06 14:16:39 -0500

    Solution

--- ECSource.java
+++ ECSource.java
@@ -1,3 +1,6 @@
+import java.util.HashSet;
+import java.util.Set;
+
 class Node<T> {
   T val;
   Node<T> next;
@@ -10,8 +13,15 @@ class Node<T> {
 
 class ECSource {
   public static <T> boolean isUnivalueList(Node<T> head) {
-    // todo
-    return true;
+    int count = 0;
+    Set<T> has_set = new HashSet<>();
+    Node<T> temp = head;
+    while(temp != null){
+      count += 1;
+      has_set.add(temp.val);
+      temp = temp.next;
+    }
+    return has_set.size() == count;
   }
 
   public static void main(String[] args) {
@@ -19,6 +29,20 @@ class ECSource {
         // z
 
         // Printing solution
-        System.out.println((ECSource.isUnivalueList(z)));
+    System.out.println((ECSource.isUnivalueList(z)));
+    Node<Integer> u = new Node<>(2);
+    Node<Integer> v = new Node<>(2);
+    Node<Integer> w = new Node<>(3);
+    Node<Integer> x = new Node<>(3);
+    Node<Integer> y = new Node<>(2);
+
+    u.next = v;
+    v.next = w;
+    w.next = x;
+    x.next = y;
+
+// 2 -> 2 -> 3 -> 3 -> 2
+
+    System.out.println(isUnivalueList(u)); // false
     }
 }
